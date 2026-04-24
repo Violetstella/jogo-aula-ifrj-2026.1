27dbcbac45d0066ba33001cb4899aa956377484a
Luiz <>
2026-04-24 16:41:32 -0300

    2404 2

--- Assets/Scripts/DestroyLixo.cs
+++ Assets/Scripts/DestroyLixo.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public class DestroyLixo : MonoBehaviour
 {
+    public LixoSpawnerControllera lixoSpawnerControllera;
     private void OnCollisionEnter(Collision collision) {
         // Optional: Check if the entering object is the Player
         if (collision.gameObject.CompareTag("Lixo")) {
             Destroy(collision.gameObject);
+            lixoSpawnerControllera.AddToPoints(-1);
         }
     }
 }
