1a3f7eee236448e368ea613f6db2bb5c0f34c5ce
lcs <103453806+Violetstella@users.noreply.github.com>
2026-05-08 16:25:03 -0300

    SOM (TA SENDO FEITO)
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- Assets/Addpoints.cs
+++ Assets/Addpoints.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Addpoints : MonoBehaviour
-{
+{ 
+    public AudioSource source;
     public LixoSpawnerControllera lixoSpawnerControllera;
     private void OnCollisionEnter(Collision collision){
         if(collision.gameObject.CompareTag("Lixo")){
             Destroy(collision.gameObject);
+            source.Play();
             lixoSpawnerControllera.AddToPoints(1);
         }
     }
